eda5f05c05f10a464a9e2b1855a25749340d0d2f
AsterBlueCow <gusstjrr10022@gmail.com>
2023-11-26 18:11:40 +0900

    openai api apply example

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
-
+    private val client = OkHttpClient()
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,5 +43,60 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        //for openapi!
+        val etQuestion = findViewById<EditText>(R.id.etQuestion)
+        val btnSubmit = findViewById<Button>(R.id.btnSubmit)
+        val txtResponse = findViewById<TextView>(R.id.txtResponse)
+
+        btnSubmit.setOnClickListener {
+            val question = etQuestion.text.toString()
+            getResponse(question){response ->
+                runOnUiThread {
+                    txtResponse.text = response
+                }
+            }
+        }
+    }
+
+    fun getResponse(question: String, callback: (String) -> Unit) {
+        val url = "https://api.openai.com/v1/engines/text-davinci-003/completions"
+        val apiKey = "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" // 여기에 고유 API 키 삽입, 만약 크레딧이 없다면 작동X
+        val requestBody = """
+            {
+            "prompt": "$question",
+            "max_tokens": 500,
+            "temperature": 0
+            }
+        """.trimIndent()
+
+        val request = Request.Builder()
+            .url(url)
+            .addHeader("Content-Type", "application/json")
+            .addHeader("Authorization", "Bearer $apiKey")
+            .post(requestBody.toRequestBody("application/json".toMediaTypeOrNull()))
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("error","OpenAPI failed", e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                if (body != null) {
+                    Log.v("data", body)
+                }
+                else {
+                    Log.v("data", "empty")
+                }
+
+                val jsonObj = JSONObject(body)
+                val jsonArray:JSONArray = jsonObj.getJSONArray("choices")
+                val txtResult = jsonArray.getJSONObject(0).getString("text")
+                callback(txtResult)
+            }
+
+        })
     }
 }
